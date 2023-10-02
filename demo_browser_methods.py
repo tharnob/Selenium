de5896458216f676cba41aea4f1c826ee90c0505
Tafsir Haque Arnob <tharnob@gmail.com>
2023-10-02 14:38:20 +0600

    Changed 1

--- demo_browser_methods.py
+++ demo_browser_methods.py
@@ -8,13 +8,24 @@ from selenium.webdriver.common.by import By
 class DemoSeleniumLearning():
     def demo_browser_method(self):
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-        driver.get("https://secure.yatra.com/")
-        lista = driver.find_elements(By.TAG_NAME, "a")
-        print(len(lista))
-        for i in lista:
-            print(i.text)
-        time.sleep(4)
+        driver.get("https://training.rcvacademy.com")
+        print(driver.current_url)
+        print(driver.title)
+        driver.maximize_window()
+        driver.fullscreen_window()
+        driver.refresh()
+        time.sleep(2)
+        driver.find_element("link text", "ALL COURSES").click()
+        driver.back()
+        time.sleep(2)
+        driver.forward()
+        time.sleep(2)
+        driver.minimize_window()
+        time.sleep(10)
+        driver.quit()
 
 
-findById = DemoFindElementByCss()
-findById.locate_by_demo()
+
+
+demoBrowser = DemoSeleniumLearning()
+demoBrowser.demo_browser_method()
