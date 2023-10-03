04c9a631f858df1847a3f2a9523a6be2c5bab3cc
Tafsir Haque Arnob <tharnob@gmail.com>
2023-10-03 20:04:56 +0600

    Updated

--- index.py
+++ index.py
@@ -2,19 +2,19 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 
 
-
-class DemoFindElementByID():
+class DemoByClass():
     def locate_by_demo(self):
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         driver.get("https://secure.yatra.com/social/common/yatra/signin.htm")
-        driver.find_element("id", "login-input").send_keys("test@test.com")
+        driver.find_element(By.CLASS_NAME, "email-login-box").send_keys("rcv@academy.com")
         time.sleep(4)
 
 
-findById = DemoFindElementByID()
+findById = DemoByClass()
 findById.locate_by_demo()
 
 
