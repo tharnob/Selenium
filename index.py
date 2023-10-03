c4631fc997d5bf86928114aca7399d0cd31ed745
Tafsir Haque Arnob <tharnob@gmail.com>
2023-10-03 20:24:26 +0600

    Get Text

--- index.py
+++ index.py
@@ -14,8 +14,33 @@ class DemoByClass():
         time.sleep(4)
 
 
-findById = DemoByClass()
-findById.locate_by_demo()
+
+class DemoGetText():
+
+    def getTheText(self):
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver.get("https://www.yatra.com/")
+        text = driver.find_element(By.XPATH, "//body/div[@class='theme-snipe']/div[@id='themeSnipe']/section[@class='wrapper']/div[@class='right_data']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/a[2]/div[1]").text
+
+        text1 = driver.find_element(By.XPATH, "//h1[contains(text(),'Book Flights, Hotels, Trains, Buses, Cruise and Ho')]").text
+
+
+
+        print(text)
+        print(text1)
+        time.sleep(2)
+
+
+
+
+
+# getText = DemoGetText()
+# getText.getTheText()
+
+
+
+# findById = DemoByClass()
+# findById.locate_by_demo()
 
 
 
