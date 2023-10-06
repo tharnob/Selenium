a81167e58e9fe4864421ee2b14becf1dbd097128
Tafsir Haque Arnob <tharnob@gmail.com>
2023-10-06 16:49:14 +0600

    Auto Suggest

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -178,15 +179,50 @@ class DemoDropDownMultiSelect:
 
 
 
-multiSelect = DemoDropDownMultiSelect()
-multiSelect.demo_dropdown_multiSelect()
+class DemoAutoSuggest:
+    def demo_auto_suggest(self):
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver.get("https://www.yatra.com/")
+        depart_from = driver.find_element(By.XPATH, "//input[@id='BE_flight_origin_city']")
+        depart_from.click()
+        time.sleep(2)
+        depart_from.send_keys("New Delhi")
+        time.sleep(2)
+        depart_from.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        going_to = driver.find_element(By.XPATH, "//input[@id='BE_flight_arrival_city']")
+        time.sleep(2)
+        going_to.send_keys("New")
+        time.sleep(2)
+        search_result = driver.find_elements(By.XPATH, "//div[@class='viewport']//div[1]//li")
+        time.sleep(2)
+        print(search_result)
+        print(len(search_result))
+        time.sleep(2)
+        for result in search_result:
+            print(result.text)
+            print(result)
+            print("||||||||||||")
+            if "New York (JFK)" in result.text:
+                result.click()
+                time.sleep(4)
+                break
+
+
+
+        
+        
+d_auto = DemoAutoSuggest()
+d_auto.demo_auto_suggest()
 
 
 
 
 
 
-
+# multiSelect = DemoDropDownMultiSelect()
+# multiSelect.demo_dropdown_multiSelect()
 
 
 # ddDemo = DemoDropdown()
