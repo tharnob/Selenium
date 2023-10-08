386b18afc572e3d2a56caf6312ab927c807fa08d
Tafsir Haque Arnob <tharnob@gmail.com>
2023-10-08 17:37:49 +0600

    Handle Alert!

--- demo_multiple_window_and_more.py
+++ demo_multiple_window_and_more.py
@@ -2,6 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.alert import Alert
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -62,8 +63,46 @@ class DemoIframe:
         time.sleep(24)
 
 
-demoFrame = DemoIframe()
-demoFrame.demo_frame()
+
+class DemoJsPopup:
+    def demo_js_alerts(self):
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver.get("https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt")
+        driver.switch_to.frame("iframeResult")
+
+        time.sleep(5)
+
+        print("Worked!")
+
+        # Accept Alert
+
+        driver.find_element(By.XPATH, "//button[normalize-space()='Try it']").click()
+        time.sleep(2)
+        driver.switch_to.alert.accept()
+        time.sleep(2)
+
+        print("Worked! AGAIN!")
+
+        # Dismiss Alert
+        time.sleep(5)
+        driver.find_element(By.XPATH, "//button[normalize-space()='Try it']").click()
+        time.sleep(2)
+        # driver.switch_to.alert.dismiss()
+        Alert(driver).dismiss()
+        time.sleep(2)
+
+        # Send text in alert
+
+        driver.find_element(By.XPATH, "//button[normalize-space()='Try it']").click()
+        time.sleep(2)
+        print(driver.switch_to.alert.text)
+        driver.switch_to.alert.send_keys("Tafsir Haque Arnob")
+        driver.switch_to.alert.accept()
+        time.sleep(2)
+
+
+jsAlerts = DemoJsPopup()
+jsAlerts.demo_js_alerts()
 
 
 
@@ -71,6 +110,10 @@ demoFrame.demo_frame()
 
 
 
+
+
+# demoFrame = DemoIframe()
+# demoFrame.demo_frame()
 
 
 # demoMultipleWindows = DemoMultipleWindows()
