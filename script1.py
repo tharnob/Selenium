cd9fb60cf4c6e7a398f038cf80eded169638d9af
Tafsir Haque Arnob <tharnob@gmail.com>
2023-10-12 05:19:29 +0600

    Pytest First Time

--- script1.py
+++ script1.py
@@ -1,9 +1,15 @@
+import time
 def do_something():
     print("Running one million lines of code")
+
+
+    a = []
+    b = len(a)
+    print(type(b))
 
 
 # if __name__ == "__main__":
 #     do_something()
 
-# do_something()
+do_something()
 
